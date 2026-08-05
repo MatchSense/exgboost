589c0275863767bef382504b0eda38cc2c9fca68
Ian Perks <ian@adaptcc.co.uk>
2026-08-05 08:18:23 +0100

    Fix precompiled nif download; tags X.Y.Z not vX.Y.Z

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule EXGBoost.MixProject do
       version: @version,
       make_precompiler: {:nif, CCPrecompiler},
       make_precompiler_url:
-        "https://github.com/iperks/exgboost/releases/download/v#{@version}/@{artefact_filename}",
+        "https://github.com/iperks/exgboost/releases/download/#{@version}/@{artefact_filename}",
       make_precompiler_priv_paths: ["libexgboost.*", "lib"],
       # NIF Versions correspond to OTP Releases
       # https://github.com/erlang/otp/blob/d3aa6c044c3927f011fb76ac087d5ce0e814954c/erts/emulator/beam/erl_nif.h#L57
